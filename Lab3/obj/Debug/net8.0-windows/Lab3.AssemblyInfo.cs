23c6a21a3ccee0c4df6f443f7f8e7555d495a1c6
unknown <juchimiuk24148@stud.akademiabialska.pl>
2025-10-17 20:51:28 +0200

    poprawki w ui

--- Lab3/obj/Debug/net8.0-windows/Lab3.AssemblyInfo.cs
+++ Lab3/obj/Debug/net8.0-windows/Lab3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4488ad4c01ded172d01bc6716557dff56b7e5a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33a981f80da2d26c69039866d24ea45278b4c416")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
