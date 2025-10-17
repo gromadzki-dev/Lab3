d8b86e8bcde1d7976ea1bfe3adf8b3bf83f3256b
Jakub Gromadzki (Uczelnia) <gromadzki24144@stud.akademiabialska.pl>
2025-10-17 21:23:53 +0200

    Fixed conflicts

---
+++ Lab3/Vessels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class Vessels
+    {
+        public string Name { get; set; } = "null";
+        public double Volume { get; set; } = 0;
+    }
+}
--- Lab3/obj/Debug/net8.0-windows/Lab3.AssemblyInfo.cs
+++ Lab3/obj/Debug/net8.0-windows/Lab3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed6c40f0085985d24943d9d0f646f273bf37a5b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c33656696e0b0e69691368d826d53ca8c02cede")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
